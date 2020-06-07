f6bd850acdd97d2821aed689d7054fa6784885e4
Israel Koren <effiko@aol.com>
2020-06-07 17:39:41 +0300

    ver 1.2

--- db_server.js
+++ db_server.js
@@ -29,7 +29,33 @@ con.connect((err) => {
     }
     console.log('1. connected as id ' + con.threadId);
 });
-
+// add a new collection to a chunk, return the collection including the collection_id
+// @input : col_json : JSON string
+// @process: create the collection, add it's ID to the chunk 
+// @output: a JSON string with embeded collection_id
+async function addNewCollection(col_json, chunk_id){
+    sql = `newCollection(${col_json})`;
+ try {
+    col_id = await doQuery(sql);
+    col_json.collection_id = col_id;
+    console.log(`col_id: ${col_id}, col_json=${col_json}`);
+    
+} catch (err) {
+    console.log(err);
+    throw err ;
+}
+sql = `update chunks set chk_json = json_append(chk_json->>'$.collections', '${col_id}') \
+chunks where chunk_id = ${chunk_id}`;
+try {
+    result = await doQuery(sql);
+console.log(result);
+return col_right;
+   
+} catch (err) {
+    console.log(err);
+    throw err ;
+}
+}
 /// getCollection(chunk_id:string, isApproved:boolean, isPlate:boolean)
 // collection.plate = isPlate וגם collection.approve = isApproved וגם onWork = false וגם status=false .
 async function getCollection(chunk_id, isApproved, isPlate) {
@@ -73,9 +99,18 @@ async function getCollection(chunk_id, isApproved, isPlate) {
 async function freeCollection(collection_id){
     try{
         sql = `update collections set colOnWork=false where collection_id='${collection_id}'`;
+        freeResult = await doQuery(sql);
+        console.log(result);
+//        return result;
+    } catch (err) {
+        console.log(err);
+        throw err ;
+    }
+    try{
+        sql = `delete from unlockonwork  where tableName='collections' and collection_id='${collection_id}'`;
         result = await doQuery(sql);
         console.log(result);
-        return result;
+        return freeResult;
     } catch (err) {
         console.log(err);
         throw err ;
@@ -93,7 +128,50 @@ async function removeImageFromCollection(in_collection_id, in_image_id){
     }
 }
 async function splitCollection(collection_id, chunk_id, index){
-    
+    sql = `select col_json from collections where collection_id = '${collection_id}'`;
+    try {
+        result = await doQuery(sql);
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+        throw err ;
+    }
+    col_right = col_json;
+    col_right.images = col_right.images.slice(index);
+    col_left  = col_json;
+    col_left.images = col_left.images.slice(0,index);
+    collection_left = addNewCollection(JSON.stringify(col_left));
+    collection_right = addNewCollection(JSON.stringify(col_right));
+    sql=`update chunks set chk_json = 
+    JSON_REMOVE(chk_json, JSON_UNQUOTE(JSON_SEARCH(chk_json->'$.collections', 'one', ${collection_id})))
+    where chunk_id = ${chunk_id}`;
+    try {
+        result = await doQuery(sql);
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+        throw err ;
+    }
+    return collection_right;
+}
+/*
+approveCollection(chunk_id:string, collection_id:string)
+משנה במקבץ: approved=true .
+משנה במקבץ: onWork=false .
+מחזיר: etCollection(chunk_id, false, false)
+
+*/
+async function approveCollection(collection_id, chunk_id){
+sql = `update collections set col_json = json_replace(col_json->>'$.approved', true)
+where collection_id = ${collection_id};`
+try {
+    result = await doQuery(sql);
+    console.log(result);
+} catch (err) {
+    console.log(err);
+    throw err ;
+}
+return getCollection(chunk_id, false, false);
 }
 //+++++++++++++++++++++++
 app.post('/getCollection', async (req, res) => {
@@ -132,7 +210,15 @@ app.post('/splitCollection', async (req, res) => {
     console.log(result);
     res.send(result);
 });
-
+// approveCollection(chunk_id:string, collection_id:string)
+app.post('/approveCollection', async (req, res) => {
+    collection_id = req.body.collection_id ;
+    chunk_id = req.body.chunk_id ;
+    index = req.body.index ;
+    result = await approveCollection(collection_id, chunk_id);
+    console.log(result);
+    res.send(result);
+});
 // ==============================================================
 
 beginTime = Date.now();
